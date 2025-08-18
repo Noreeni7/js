a66de1b8f19eb4f1ea0cfc23ef528076a3b72d52
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-08-18 11:45:01 +0300

    update

--- script.js
+++ script.js
@@ -1,66 +1,155 @@
 console.log("hi");
 // document.querySelector(".message").textContent = "Day 1 exercise complete!"
 
-//====== declaring variables ========
-function showVariables(){
-let myName = "Richard";
-const birthYear = 1960;
-let likeJS = true;
-let age = 2025 - birthYear;
-let sentence =
-  "My name is " +
-  myName +
-  "." +
-  " I am " +
-  age +
-  " years old, and it is " +
-  likeJS +
-  " that I like JavaScript.";
-console.log(sentence);
-document.querySelector(".message").textContent = sentence + "\n\n";
+//=========================== DECLARING VARIABLES ==================
+function showVariables() {
+  let myName = "Richard";
+  const birthYear = 1960;
+  let likeJS = true;
+  let age = 2025 - birthYear;
+  let sentence =
+    "My name is " +
+    myName +
+    "." +
+    " I am " +
+    age +
+    " years old, and it is " +
+    likeJS +
+    " that I like JavaScript.";
+  console.log(sentence);
+  document.querySelector(".message").textContent = sentence + "\n";
 }
 
-
-// ===== operators ======
-function showOperators(){
-let num1 = 10;
-let num2 = 3;
-let sum = num1 + num2;
-let difference = num1 - num2;
-let product = num1 * num2;
-let sentences =
-  "The sum of 10 and 3 is " +
-  sum +
-  "," +
-  "The difference between num1 and num2 is " +
-  difference +
-  "\nProduct:" +
-  product;
-console.log(sentences);
-document.querySelector(".message").textContent += sentences + "\n\n";
+// ============================ OPERATORS =============================
+function showOperators() {
+  let num1 = 10;
+  let num2 = 3;
+  let sum = num1 + num2;
+  let difference = num1 - num2;
+  let product = num1 * num2;
+  let sentences =
+    "The sum of 10 and 3 is " +
+    sum +
+    "," +
+    "The difference between num1 and num2 is " +
+    difference +
+    "\nProduct:" +
+    product;
+  console.log(sentences);
+  document.querySelector(".message").textContent += sentences + "\n";
 }
 
-
-// ====== conditional statements ========
-function showConditionals(){
-let age = 20;
-let country = "Kenya";
-let message = "";
-if (age >= 18) {
-  message += "adult\n";
-} else if (age < 18) {
-  message += "minor\n";
+// ========================= CONDITIONAL STATEMENTS ===========================
+function showConditionals() {
+  let age = 20;
+  let country = "Kenya";
+  let message = "";
+  if (age >= 18) {
+    message += "adult\n";
+  } else if (age < 18) {
+    message += "minor\n";
+  }
+  if (country === "Kenya") {
+    message += "Kenyan";
+  }
+  console.log(message);
+  document.querySelector(".message").textContent += message + "\n";
 }
-if (country === "Kenya") {
-  message += "Kenyan";
-}
-console.log(message);
-document.querySelector(".message").textContent += message + "\n\n";
+
+// =============================== FOR LOOP ===============================
+function showForloop() {
+  // ===== counting forward ======
+  for (let i = 1; i <= 10; i++) {
+    console.log("Number:", i);
+    document.querySelector(".message").textContent += "Number: " + i + "\n";
+  }
+
+  // ===== counting downward ======
+  for (let i = 10; i >= 0; i--) {
+    console.log("Number: ", i);
+    document.querySelector(".message").textContent += "Number: " + i + "\n";
+  }
 }
 
+// =============================== WHILE LOOP =============================
+function showWhileloop() {
+  // =========== counting forward ===========
+  let i = 0;
+  while (i <= 10) {
+    if (i % 2 === 0) {
+      console.log("Even Number:", i);
+      document.querySelector(".message").textContent +=
+        "Even Number: " + i + "\n";
+    } else {
+      console.log("Odd Number:", i);
+      document.querySelector(".message").textContent +=
+        "Odd Number:" + i + "\n";
+    }
+    i++;
+  }
 
-// --------- calling functions ---------
+  // ============== counting downward ===========
+  i = 10;
+  while (i >= 0) {
+    if (i % 2 == 0) {
+      console.log("Even Number: ", i);
+      document.querySelector(".message").textContent +=
+        "Even Number: " + i + "\n";
+    } else {
+      console.log("Odd Number: ", i);
+      document.querySelector(".message").textContent +=
+        "Odd Number: " + i + "\n";
+    }
+    i--;
+  }
+}
+
+// ============================ DO...WHILE LOOP ================================
+function showDoWhileLoop() {
+  let i = 1;
+  do {
+    if (i % 3 == 0) {
+      console.log("Number divisible by 3: ", i);
+      document.querySelector(".message").textContent +=
+        "Number divisible by 3: " + i + "\n";
+    } else {
+      console.log("Number: ", i);
+      document.querySelector(".message").textContent += "Number: " + i + "\n";
+    }
+    i++;
+  } while (i <= 10);
+}
+
+// ==================== Events & DOM Interaction ======================
+// ==== check user age ====
+document.getElementById("checkAge").addEventListener("click", function () {
+  const age = Number(document.getElementById("ageInput").value);
+  let message = "";
+
+  if (age === 21) {
+    message = "Happy 21st birthday!";
+  } else if (age <= 18) {
+    message = "You are a minor";
+  } else {
+    message = "You are an adult";
+  }
+  console.log(message);
+  document.querySelector(".message").textContent = message;
+});
+
+// ==== greet user =====
+document.getElementById("checkName").addEventListener("click", function(){
+  const name = document.getElementById("nameInput").value;
+  message = "Hello " + name
+  console.log(message);
+  document.querySelector(".message").textContent = message;
+})
+
+
+// -------------------------- CALLING FUNCTIONS -------------------------
 showVariables();
 showOperators();
 showConditionals();
-
+showForloop();
+showWhileloop();
+showDoWhileLoop();
